52b0530c4ae823bd5105fdf8bda9e0f74c5d550f
Jon Friskics <jonfriskics@gmail.com>
2017-02-28 13:48:06 -0500

    add in-memory voting logic, bodyParser, ejs, and index template

--- src/server.js
+++ src/server.js
@@ -1,14 +1,39 @@
 'use strict';
 
 const express = require('express');
+let bodyParser = require('body-parser');
+let ejs = require('ejs');
+
 let votes = {
   sandwiches: 0,
   tacos: 0
 };
 
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+
+
 app.get('/', function (req, res) {
-  res.send('Current vote count\nSandwiches: ' + votes.sandwiches + '\nTacos: ' + votes.tacos + '\n');
+  res.render('pages/index', {
+    votes: votes
+  });
+});
+
+app.post('/vote', urlencodedParser, function(req, res) {
+  let vote = req.body.yourVote;
+  console.log('vote is: ' + vote);
+  if(vote == 'sandwiches') {
+    votes.sandwiches = votes.sandwiches + 1;
+  } else if(vote == 'tacos') {
+    votes.tacos = votes.tacos + 1;
+  } else {
+    res.send('Something went wrong: vote contains ' + vote);
+  }
+  console.log(votes.sandwiches + ' ' + votes.tacos);
+  res.redirect('/');
 });
 
 const PORT = 8888;
