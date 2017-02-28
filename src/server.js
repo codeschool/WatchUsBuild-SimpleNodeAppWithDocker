c23388fd983c4e5b2222316630e8fe8bcec1bbc6
Jon Friskics <jonfriskics@gmail.com>
2017-02-28 16:33:37 -0500

    first attempt at connecting postgres

--- src/server.js
+++ src/server.js
@@ -1,8 +1,31 @@
 'use strict';
 
 const express = require('express');
+const app = express();
 let bodyParser = require('body-parser');
 let ejs = require('ejs');
+let pg = require('pg');
+
+let client = new pg.Client('postgres://postgres:blahblah@localhost:9000');
+client.connect(function (err) {
+  if (err) throw err;
+  // client.query('CREATE TABLE IF NOT EXISTS votes (string string timestamptz)', function(err, result) {
+  //   if(err) throw err;
+  //   console.log(result);
+  // });
+  // client.query('DELETE FROM votes', function(err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
+  // client.query('INSERT INTO votes (string string) VALUES ($1)', "")
+  client.query('SELECT number_of_votes FROM votes WHERE option_name="$1"', ['sandwiches'], function (err, result) {
+    if (err) throw err;
+    console.log(result.rows[0]); // outputs: { name: 'brianc' } 
+    client.end(function (err) {
+      if (err) throw err;
+    });
+  });
+});
 
 let votes = {
   sandwiches: 0,
@@ -11,10 +34,8 @@ let votes = {
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
-
 
 app.get('/', function (req, res) {
   res.render('pages/index', {
@@ -24,15 +45,13 @@ app.get('/', function (req, res) {
 
 app.post('/vote', urlencodedParser, function(req, res) {
   let vote = req.body.yourVote;
-  console.log('vote is: ' + vote);
-  if(vote == 'sandwiches') {
+  if(vote === 'sandwiches') {
     votes.sandwiches = votes.sandwiches + 1;
-  } else if(vote == 'tacos') {
+  } else if(vote === 'tacos') {
     votes.tacos = votes.tacos + 1;
   } else {
-    res.send('Something went wrong: vote contains ' + vote);
+    console.log('Something went wrong: vote contains ' + vote);
   }
-  console.log(votes.sandwiches + ' ' + votes.tacos);
   res.redirect('/');
 });
 
